8aae4a65f240b23a6d46d3bdfb21cbcc3b1d4046
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2020-12-12 01:33:39 +0900

    Add test programs of JNA and LED

---
+++ test/sample_JNA/Hello.java
@@ -0,0 +1,14 @@
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+public class Hello {
+    public interface CLibrary extends Library {
+        CLibrary INSTANCE = (CLibrary) Native.loadLibrary("c", CLibrary.class);
+
+        void printf(String format, Object... args);
+    }
+
+    public static void main(String[] args) {
+        CLibrary.INSTANCE.printf("Hello, World!!\n");
+    }
+}
---
+++ test/sample_LED/sample02-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int run(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+    printf("%d\n", run(argc, argv));
+
+    return 0;
+}
